e027f75e804564184015497f0f53a2108334e0fd
Andrea <Andrea@halls-10-40-78-10.wireless.leeds.ac.uk>
2017-11-18 21:48:52 +0000

    Initial Commit

---
+++ Dot to Dot v.1.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dot to Dot v.1.0
+//
+//  Created by Andrea on 18/11/17.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dot to Dot v.1.0/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dot to Dot v.1.0
+//
+//  Created by Andrea on 18/11/17.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
