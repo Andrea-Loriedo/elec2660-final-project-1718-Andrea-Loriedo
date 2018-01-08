5e55f3752fc27f9c1f8f63211ef684e3cda6fb1d
Andrea <Andrea@halls-10-40-75-17.wireless.leeds.ac.uk>
2018-01-08 18:27:38 +0000

    Added a (still empty) table view to display trial data

---
+++ Dot to Dot v.1.0/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Dot to Dot v.1.0
+//
+//  Created by Andrea on 8/1/18.
+//  Copyright © 2018 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
